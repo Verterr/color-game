88a5aad4a696e6ac4c3c1dd7f012e2bc733a4bf5
Jan Kaczmarek <janjakubkaczmarek@gmail.com>
2020-10-23 10:36:29 +0200

    implement basic react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, Redirect} from "react-router-dom";
+import MainPage from "./Components/MainPage/MainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  let routes = (
+      <Switch>
+        <Route to="/" component={MainPage}/>
+        <Redirect to="/" />
+      </Switch>
   );
+  return (
+      <div className="App">
+          {routes}
+      </div>
+  )
 }
-
-export default App;
