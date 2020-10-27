a0b97ffb28f442b65fe4ff5ac84071a030ae4bec
Jan Kaczmarek <janjakubkaczmarek@gmail.com>
2020-10-27 22:23:31 +0100

    added color generator

--- src/Components/CardGrid/CardGrid.js
+++ src/Components/CardGrid/CardGrid.js
@@ -7,19 +7,38 @@ import {Card} from "@material-ui/core";
 const Color = styled(Card)`
     width: 50px;
     height: 50px;
+    background: ${props => props.color} !important;
     `;
 
 class CardGrid extends Component {
+
+    colorGenerator = () => {
+        let color = '';
+        const num = Math.random();
+        if(num <= 0.25){
+            color = "yellow";
+        } else if (num <= 0.5) {
+            color = "green";
+        } else if (num <= 0.75) {
+            color = "red";
+        } else if (num <= 1) {
+            color = "orange";
+        }
+        return color;
+    }
+
     cardList = [];
     renderCards = () => {
         for (let i = 0; i < Math.pow(this.props.gameSize, 2); i++){
-            this.cardList.push(<Color key={i}/>)
+            const color = this.colorGenerator();
+            this.cardList.push(<Color key={i} color={color}/>)
+            console.log(this.cardList);
         }
     }
 
     render() {
         this.renderCards();
-        return this.cardList.map(card => <Color key={card.key}/>)
+        return this.cardList.map(card => <Color key={card.key} color={card.props.color}/>)
     }
 }
 
